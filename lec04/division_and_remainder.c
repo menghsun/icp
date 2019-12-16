ee741b98d0461de81867fc1ade5843acf7cf7cc4
menghsun <tsaimh@gmail.com>
2019-12-16 10:34:48 +0800

    add division_and_remainder.c

---
+++ lec04/division_and_remainder.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(void)
+{
+	int x;
+	
+//	x = 10 % 3.0;
+	printf("1 / 2 = %d\n", 1/2);
+	printf("2 / 0 = %d\n", 2/0);
+	printf("2.0 / 0 = %d\n", 2.0/0);
+	printf("1 %% 2 = %d\n", 1%2);
+//	printf("10 %% 3.0 = %d\n", 10%3.0);
+	return 0;
+}
